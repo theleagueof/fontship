d6b936db208839f2d3b2569a4a7fe0424246319f
Caleb Maclennan <caleb@alerque.com>
2020-08-12 22:15:10 +0300

    chore: Add build hints for git archive builds, speedup otherwise

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,15 @@
 #/usr/bin/env sh
+set -e
+
+# Hint how to build from Github's snapshot archives
+if [ ! -e ".git" ]; then
+    if [ ! -f ".tarball-version" ]; then
+    incomplete_source "No version information found" \
+        "identify the correct version with \`echo \$version > .tarball-version\`"
+    fi
+else
+    # Save a ./configure cycle with a headstart for fresh clones
+    ./build-aux/git-version-gen .tarball-version > .version
+fi
+
 autoreconf --install -W none
