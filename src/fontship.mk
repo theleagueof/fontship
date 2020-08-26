0f4320191388096e387e19e2e488dbe173aacc84
Caleb Maclennan <caleb@alerque.com>
2020-08-26 11:54:26 +0300

    feat(rules): Split makefile into init & actually processing
    This complexity is kind of needed to allow projects to have custom
    rules. Without it they don't have any access to the anything Fontship
    initializes. They won't even be running with the same shell options as
    Fontship itself, at least not on first pass.
    
    This normalizes it so all the makefiles are on a level field.

---
+++ src/fontship.mk
@@ -0,0 +1,48 @@
+# Defalut to running jobs in parallel, one for each CPU core
+MAKEFLAGS += --jobs=$(shell nproc) --output-sync=target
+# Default to not echoing commands before running
+MAKEFLAGS += --silent
+# Disable as much built in file type builds as possible
+MAKEFLAGS += --no-builtin-rules
+.SUFFIXES:
+
+# Run recipies in zsh, and all in one pass
+SHELL := zsh
+.SHELLFLAGS := +o nomatch -e -c
+.ONESHELL:
+.SECONDEXPANSION:
+
+# Don't drop intermediate artifacts (saves rebulid time and aids debugging)
+.SECONDARY:
+.PRECIOUS: %
+.DELETE_ON_ERROR:
+
+CONTAINERIZED != test -f /.dockerenv && echo true || echo false
+
+# Initial environment setup
+FONTSHIPDIR != cd "$(shell dirname $(lastword $(MAKEFILE_LIST)))/" && pwd
+GITNAME := $(notdir $(or $(shell git remote get-url origin 2> /dev/null | sed 's,^.*/,,;s,.git$$,,' ||:),$(shell git worktree list | head -n1 | awk '{print $$1}')))
+PROJECT ?= $(shell $(PYTHON) $(PYTHONFLAGS) -c 'import re; print(re.sub(r"[-_]", " ", "$(GITNAME)".title()).replace(" ", ""))')
+_PROJECTDIR != cd "$(shell dirname $(firstword $(MAKEFILE_LIST)))/" && pwd
+PROJECTDIR ?= $(_PROJECTDIR)
+PUBDIR ?= $(PROJECTDIR)/pub
+SOURCEDIR ?= sources
+
+# Some Makefile shinanigans to avoid aggressive trimming
+space := $() $()
+
+# Allow overriding executables used
+FONTMAKE ?= fontmake
+FONTV ?= font-v
+GFTOOLS ?= gftools
+PYTHON ?= python3
+SFNT2WOFF ?= sfnt2woff-zopfli
+TTFAUTOHINT ?= ttfautohint
+PSAUTOHINT ?= psautohint
+SFDNORMALIZE ?= sfdnormalize
+TTX ?= ttx
+WOFF2COMPRESS ?= woff2_compress
+
+include $(FONTSHIPDIR)/functions.mk
+
+
--- src/rules.mk
+++ src/rules.mk
@@ -1,49 +1,10 @@
-# Defalut to running jobs in parallel, one for each CPU core
-MAKEFLAGS += --jobs=$(shell nproc) --output-sync=target
-# Default to not echoing commands before running
-MAKEFLAGS += --silent
-# Disable as much built in file type builds as possible
-MAKEFLAGS += --no-builtin-rules
-.SUFFIXES:
-
-# Run recipies in zsh, and all in one pass
-SHELL := zsh
-.SHELLFLAGS := +o nomatch -e -c
-.ONESHELL:
-.SECONDEXPANSION:
-
-# Don't drop intermediate artifacts (saves rebulid time and aids debugging)
-.SECONDARY:
-.PRECIOUS: %
-.DELETE_ON_ERROR:
-
-CONTAINERIZED != test -f /.dockerenv && echo true || echo false
-
-# Initial environment setup
-FONTSHIPDIR != cd "$(shell dirname $(lastword $(MAKEFILE_LIST)))/" && pwd
-GITNAME := $(notdir $(or $(shell git remote get-url origin 2> /dev/null | sed 's,^.*/,,;s,.git$$,,' ||:),$(shell git worktree list | head -n1 | awk '{print $$1}')))
-PROJECT ?= $(shell $(PYTHON) $(PYTHONFLAGS) -c 'import re; print(re.sub(r"[-_]", " ", "$(GITNAME)".title()).replace(" ", ""))')
-_PROJECTDIR != cd "$(shell dirname $(firstword $(MAKEFILE_LIST)))/" && pwd
-PROJECTDIR ?= $(_PROJECTDIR)
-PUBDIR ?= $(PROJECTDIR)/pub
-SOURCEDIR ?= sources
-
-# Some Makefile shinanigans to avoid aggressive trimming
-space := $() $()
-
-# Allow overriding executables used
-FONTMAKE ?= fontmake
-FONTV ?= font-v
-GFTOOLS ?= gftools
-PYTHON ?= python3
-SFNT2WOFF ?= sfnt2woff-zopfli
-TTFAUTOHINT ?= ttfautohint
-PSAUTOHINT ?= psautohint
-SFDNORMALIZE ?= sfdnormalize
-TTX ?= ttx
-WOFF2COMPRESS ?= woff2_compress
-
-include $(FONTSHIPDIR)/functions.mk
+# If called using the fontship CLI the init rules will be sources before any
+# project specific ones, then everything will be sourced in order. If people do
+# a manual include to rules they may or may not know to source the
+# initilazation rules first. this is to warn them.
+ifeq ($(FONTSHIPDIR),)
+$(error Please initialize Fontship by sourcing fontship.mk first, then include your project rules, then source this rules.mk file)
+endif
 
 SOURCES ?= $(shell git ls-files -- '$(SOURCEDIR)/*.glyphs' '$(SOURCEDIR)/*.sfd' '$(SOURCEDIR)/*.ufo/*' '$(SOURCEDIR)/*.designspace' | sed -e '/\.ufo/s,.ufo/.*,.ufo,' | uniq)
 SOURCES_SFD ?= $(filter %.sfd,$(SOURCES))
