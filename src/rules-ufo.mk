d649c3d6315d93b21afe4615bcd81ae96aff96e7
Caleb Maclennan <caleb@alerque.com>
2020-08-26 15:11:02 +0300

    chore(ufo): Always use normalized sources in build steps

--- src/rules-ufo.mk
+++ src/rules-ufo.mk
@@ -17,14 +17,14 @@ $(BUILDDIR)/%-normalized.ufo: $(SOURCEDIR)/%.ufo $$(ufoParts) | $(BUILDDIR)
 # UFO -> OTF
 
 define otf_instance_template ?=
-$$(BUILDDIR)/$1-%-instance.otf: $(SOURCEDIR)/$1-%.ufo | $$(BUILDDIR)
+$$(BUILDDIR)/$1-%-instance.otf: $(BUILDDIR)/$1-%-normalized.ufo | $$(BUILDDIR)
 	$$(FONTMAKE) $$(FONTMAKEFLAGS) -u $$< -o otf --output-path $$@
 endef
 
 # UFO -> TTF
 
 define ttf_instance_template ?=
-$$(BUILDDIR)/$1-%-instance.ttf: $(SOURCEDIR)/$1-%.ufo | $$(BUILDDIR)
+$$(BUILDDIR)/$1-%-instance.ttf: $(BUILDDIR)/$1-%-normalized.ufo | $$(BUILDDIR)
 	$$(FONTMAKE) $$(FONTMAKEFLAGS) -u $$< -o ttf --output-path $$@
 	$$(GFTOOLS) $$(GFTOOLSFLAGS) fix-dsig --autofix $$@
 endef
