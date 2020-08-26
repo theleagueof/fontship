25cb71bc64c780c98685fac0113e36fd7a45b4cb
Caleb Maclennan <caleb@alerque.com>
2020-08-27 01:35:37 +0300

    feat(ufo): Add variable TTF output

--- src/rules-ufo.mk
+++ src/rules-ufo.mk
@@ -1,6 +1,7 @@
 ufoNormalize ?= $(UFONORMALIZER) $(UFONORMALIZERFLAGS) "$1" -o "$2"
 expandUFOParts = $(shell find "$1" -type f 2> /dev/null)
 ufoParts = $(call expandUFOParts,$(patsubst %-normalized.ufo,%.ufo,$(patsubst $(BUILDDIR)/%,$(SOURCEDIR)/%,$@)))
+styleToDS = $(_DSF_$(subst -,,$*))
 
 define makeVars =
 from fontTools.designspaceLib import DesignSpaceDocument
@@ -15,6 +16,23 @@ $(eval $(file < $(_TMP)))
 $(shell rm -f $(_TMP))
 endif
 
+$(BUILDDIR)/%-VF.ttf: $(SOURCEDIR)/%.designspace | $(BUILDDIR)
+	$(FONTMAKE) $(FONTMAKEFLAGS) -m "$<" -o variable --output-path $@
+	$(GFTOOLS) $(GFTOOLSFLAGS) fix-vf-meta $@
+	$(GFTOOLS) $(GFTOOLSFLAGS) fix-unwanted-tables --tables MVAR $@ ||:
+	$(GFTOOLS) $(GFTOOLSFLAGS) fix-dsig -f $@
+
+$(BUILDDIR)/%-hinted.ttf: $(BUILDDIR)/%.ttf
+	$(TTFAUTOHINT) $(TTFAUTOHINTFLAGS) -n $< $@
+
+$(BUILDDIR)/%-hinted.ttf.fix: $(BUILDDIR)/%-hinted.ttf
+	$(GFTOOLS) $(GFTOOLSFLAGS) fix-hinting $<
+	$(GFTOOLS) $(GFTOOLSFLAGS) fix-gasp $@
+
+$(VARIABLETTFS): %.ttf: $(BUILDDIR)/%-hinted.ttf.fix $(BUILDDIR)/last-commit
+	cp $< $@
+	$(normalizeVersion)
+
 $(SOURCEDIR)/%.ufo: UFONORMALIZERFLAGS += -m
 $(SOURCEDIR)/%.ufo: $$(call ifTrue,$$(NORMALIZE_MODE),force) | $(BUILDDIR)
 	local _normalized=$(BUILDDIR)/$(*F)-normalized.ufo
@@ -26,8 +44,6 @@ $(BUILDDIR)/%-normalized.ufo: UFONORMALIZERFLAGS += -a
 $(BUILDDIR)/%-normalized.ufo: $(SOURCEDIR)/%.ufo $$(ufoParts) | $(BUILDDIR)
 	$(call ufoNormalize,$<,$@)
 	touch $@
-
-styleToDS = $(_DSF_$(subst -,,$*))
 
 # TODO: When upstream fontmake bug gets fixed do something sane here...
 # https://github.com/googlefonts/fontmake/issues/693
