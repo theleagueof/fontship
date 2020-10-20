cc480783eb691daa1626c562801094889c1ac106
Caleb Maclennan <caleb@alerque.com>
2020-10-20 16:34:55 +0300

    fix(build): Add used but undefined error function

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 set -e
 
+incomplete_source () {
+    echo -e "$1. Please either:\n" \
+            "* $2,\n" \
+            "* or use the source packages instead of a repo archive\n" \
+            "* or use a full Git clone.\n" >&2
+    exit 1
+}
+
 # Hint how to build from Github's snapshot archives
 if [ ! -e ".git" ]; then
     if [ ! -f ".tarball-version" ]; then
