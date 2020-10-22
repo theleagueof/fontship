ff2b108b0577f50da19c80df675aeb0c35073494
Caleb Maclennan <caleb@alerque.com>
2020-10-22 22:26:15 +0300

    [NFY] fifo stuff

--- src/make/mod.rs
+++ src/make/mod.rs
@@ -1,8 +1,10 @@
 use crate::CONFIG;
 use crate::{status, CONFIGURE_DATADIR};
+// use nix::{sys, unistd};
 use std::io::prelude::*;
 use std::{error, ffi::OsString, io, result};
 use subprocess::{Exec, Redirection};
+// use tempfile::Builder;
 
 type Result<T> = result::Result<T, Box<dyn error::Error>>;
 
@@ -39,11 +41,25 @@ pub fn run(target: Vec<String>) -> Result<()> {
     };
     let repo = status::get_repo()?;
     let workdir = repo.workdir().unwrap();
+    // let fifo_dir = Builder::new()
+    //     .prefix("fontship-")
+    //     .suffix(".fifo")
+    //     .rand_bytes(5)
+    //     .tempdir()?;
+    // let fifo_path = fifo_dir.path().join("pid");
+    // unistd::mkfifo(&fifo_path, sys::stat::Mode::S_IRWXU)?;
+    // process = process.cwd(workdir).env("FONTSHIP_FIFO", fifo_path);
     process = process.cwd(workdir);
+
+    // let stdout = process.capture()?.stdout_str();
+    // eprintln!("STDOUT was {}", stdout.len());
+
     let out = process.stderr(Redirection::Merge).stream_stdout()?;
     let buf = io::BufReader::new(out);
     for line in buf.lines() {
         crate::show_line(line.unwrap());
     }
+
+    // process.join()?;
     Ok(())
 }
